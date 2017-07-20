55eb618d92f6864d7f4b9da9d160cdef658a29dc
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2017-07-20 12:40:26 +0200

    implement the discovery service

---
+++ settings/common.py
@@ -0,0 +1,14 @@
+# FQDN for current machine
+LIQUID_DOMAIN = "example.liquid"
+
+# DNS interface that the DNS server runs on
+DNSMASQ_INTERFACE = None
+
+# Default location of dns conf file to be generated
+DNSMASQ_CONFIG_PATH = "/var/lib/liquid/conf/discover/dnsmasq.conf"
+
+# Default HTTP port
+HTTP_PORT = '13777'
+
+# Default DNS upstream servers (OpenDNS ips)
+DNS_SERVERS = ['208.67.222.222', '208.67.220.220']
